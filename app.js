9f929f41ea72bb3d7234f233e918be52a3ec73b6
Michael Neely <michael@michaelneely.net>
2023-10-20 10:53:26 -0400

    Added a factory fucntion example

--- app.js
+++ app.js
@@ -34,3 +34,20 @@ printHobbies(hobbies);
 
 // Factory functions - a function that produces another function
 
+function createTaxCalculator(tax) {
+    function calculateTax(amount) {
+        return amount * tax;
+    }
+    return calculateTax;
+}
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
+
+
+
+const vatAmount = calculateTax(100, 0.19);
+const incomeTax = calculateTax(100, .20);
