20d71875d3140df514660f7591c7ea93e433d542
Michael Neely <michael@michaelneely.net>
2023-10-22 14:39:34 -0400

    Recursion lesson

--- app.js
+++ app.js
@@ -62,3 +62,24 @@ let name = 'Fred';
 userName = 'Patsy';
 
 greetUser();
+
+// function powerOf(x, n) {
+//     let result =1;
+//
+//     for( let i =0; i < n; i++) {
+//         result *= x;
+//     }
+//
+//     return result;
+// }
+
+// powerOf function using recursion
+
+function powerOf(x, n) {
+    if (n ===1) {
+        return x;
+    }
+    return x * powerOf(x, n-1);
+}
+
+console.log(powerOf(2,3));
